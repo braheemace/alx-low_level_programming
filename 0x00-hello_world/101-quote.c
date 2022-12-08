363d709fc8e065f9160fe6360056d2d695bb3f3e
ibrahim <braheemace@gmail.com>
2022-12-08 11:38:36 -0800

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-9");
 	return (1);
 }
