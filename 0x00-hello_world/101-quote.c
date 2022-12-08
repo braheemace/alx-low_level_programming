ab62ce81b432fabd98c4bb68381cc51b39ef8bc0
ibrahim <braheemace@gmail.com>
2022-12-08 11:31:33 -0800

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <conio.h>
+#include <stdio.h>
 /**
  * main - line of code using fprintf
  * code by braheem
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-	getch("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
