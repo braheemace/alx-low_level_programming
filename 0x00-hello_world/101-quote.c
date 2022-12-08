8e92e12d466d552c5a0863e0c2f68da70dde06e3
ibrahim <braheemace@gmail.com>
2022-12-08 13:34:31 -0800

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - line of code using scanf
+ * main - line of code using write
  * code by braheem
  * Return: 1
 */
 int main(void)
 {
-	vfprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-9\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-9\n", 59);
 	return (1);
 }
