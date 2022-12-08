4feb4fedbebd0d1119c57dbd24ee3abdbbfb0afa
ibrahim <braheemace@gmail.com>
2022-12-08 11:22:57 -0800

    task101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - line of code using fprintf
+ * code by braheem
+ * Return: 1
+*/
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
