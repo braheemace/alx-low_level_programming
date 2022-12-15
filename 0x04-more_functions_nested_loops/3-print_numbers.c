5947ea5ca258e707cf970f763ed5cb9bc8f8365b
ibrahim <braheemace@gmail.com>
2022-12-15 10:03:49 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints num rom 0-9
- *
- * Return: void
- */
+*print_numbers - print 0123456789
+*
+*Return: void
+*/
 
 void print_numbers(void)
 {
 	char c;
 
-	for (c = "0"; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 	_putchar(c);
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
