7ffeb54559b0adc4cf141f431599319784041ebd
ibrahim <braheemace@gmail.com>
2022-12-22 10:06:43 -0800

    tak0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *_strcat - concat 2 string
+ * _strcat - concat 2 string
  * @dest:char
  * @src:char
- * @Return:char
+ * Return:char
  */
 char *_strcat(char *dest, char *src)
 {
 	char *s = dest;
-	
+
 	while (*dest != '\0')
 	{
 		dest++;
